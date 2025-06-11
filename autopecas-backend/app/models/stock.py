1ca428e7b3d3a7d1f12886119c7217fef8f50f20
Lucas Martini <90645886+LucasMartini13@users.noreply.github.com>
2025-06-11 20:03:27 -0300

    Delete autopecas-backend directory

--- autopecas-backend/app/models/stock.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.db.database import Base
-
-class Stock(Base):
-    __tablename__ = "stock"
-    id = Column(Integer, primary_key=True, index=True)
-    part_name = Column(String, index=True)
-    quantity = Column(Integer)
